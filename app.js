24068613f45190ae8cb93679d5eca052ba57b932
Sayhan Ahmed <sayhan@ewnbd.com>
2025-05-16 01:56:33 +0600

    cookie parser added

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 import express from "express";
-import { notFoundErr } from "./middlewar/errorHandler.js";
+import cookieParser from "cookie-parser";
+import { globalErrorHandler, notFoundErr } from "./middlewar/errorHandler.js";
 
 export const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+
+
+
+
 // 404 error handler ---
 app.use(notFoundErr);
+
+// global error handler --
+app.use(globalErrorHandler);
